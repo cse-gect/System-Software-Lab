1bcfbdee9048d4b9e95e129310f3c679528b6e4a
shaheemMPM <shaheem.mpm@gmail.com>
2019-08-15 02:36:34 +0530

    Update Scheduling Algorithms Program

---
+++ 1_CPU SCHEDULING ALGORITHMS/README.md
@@ -0,0 +1,24 @@
+### CPU Scheduling Algorithms
+
+------------
+
+#### Experiment : 1
+***Aim :*** Simulate the following non-preemptive CPU scheduling algorithms to find turnaround time and waiting time.
+- FCFS
+- SJF
+- Round Robin (pre-emptive)
+- Priority
+
+
+------------
+
+##### Sample Test Case
+[![](https://github.com/cse-gect/System-Software-Lab/.github/images/)](https://github.com/cse-gect/System-Software-Lab/.github/images/)
+
+
+------------
+
+
+###### Output
+
+[![](https://github.com/cse-gect/System-Software-Lab/.github/images/)](https://github.com/cse-gect/System-Software-Lab/.github/images/)
--- 1_CPU SCHEDULING ALGORITHMS/SchedulingAlgorithms.c
+++ 1_CPU SCHEDULING ALGORITHMS/SchedulingAlgorithms.c
@@ -1,110 +1,145 @@
 #include<stdio.h>
 
 struct process{
-  int pid;
+  int index;
+  char pid[5];
   int bt;
   int at;
   int pt;
-};
+  int tt;
+  int wt;
+  int done;
+} pr[10];
+
+void clearScreen();
+void readProcess(int);
+void sortByAt(int);
+void sortByIndex(int);
+void printPorcess(int);
+void fcfs(int);
+void sjf(int);
+void roundRobin(int, int);
+void priority(int);
 
 int main(){
-  // Initializing Variables
-  struct process prt[10], tempProc;
-  int n, twt[10], tat[10], sm, wtTot, taTot;
-  float awt, atat;
-  // Printing Welcome Note
-  printf("\e[1;1H\e[2J");
-  printf("\nCPU Scheduling Algorithms\n-----------------------\n\n");
-  // Reading Input Values
-  printf("Enter Number of Processes : ");
+  int n, qt;
+  clearScreen();
+  printf("\n\nCPU Scheduling Algorithms\n");
+  printf("=========================\n\n");
+  printf("Enter Number of Process : ");
   scanf("%d", &n);
+  printf("\nEnter Time Quantum for Round Robin : ");
+  scanf("%d", &qt);
+  readProcess(n);
+  clearScreen();
+  printPorcess(n);
+  fcfs(n);
+  sjf(n);
+  roundRobin(n, qt);
+  priority(n);
+  printf("\n\n\n");
+  return 0;
+}
 
-  for (int i = 0; i < n; i++) {
-    printf("\n\nEnter Process Id of Process %d : ", i+1);
-    scanf("%d", &prt[i].pid);
-    printf("\nEnter Arrival time of Process %d : ", i+1);
-    scanf("%d", &prt[i].at);
-    printf("\nEnter Burst time of Process %d : ", i+1);
-    scanf("%d", &prt[i].bt);
-    printf("\nEnter Priority of Process %d : ", i+1);
-    scanf("%d", &prt[i].pt);
+void clearScreen(){
+  printf("\e[1;1H\e[2J");
+}
+
+void readProcess(int n){
+  for (int i = 0; i < n; i++){
+    pr[i].index = i;
+    printf("\n\nEnter Process Name : ");
+    scanf("%s", pr[i].pid);
+    printf("Enter Arrival Time (AT) of %s : ", pr[i].pid);
+    scanf("%d", &pr[i].at);
+    printf("Enter Burst Time (BT) of %s : ", pr[i].pid);
+    scanf("%d", &pr[i].bt);
+    printf("Enter Priority of %s : ", pr[i].pid);
+    scanf("%d", &pr[i].pt);
+    pr[i].wt = 0;
+    pr[i].tt = 0;
   }
-  // Sorting Processes on Arrival Order
+}
+
+void sortByAt(int n){
+  int sm;
+  struct process tempProc;
   for (int i = 0; i < n-1; i++){
     sm = i;
     for (int j = i+1; j < n; j++){
-      if (prt[j].at < prt[sm].at) {
+      if (pr[j].at < pr[sm].at) {
         sm = j;
       }
     }
     if (sm != i) {
-      tempProc = prt[i];
-      prt[i] = prt[sm];
-      prt[sm] = tempProc;
+      tempProc = pr[i];
+      pr[i] = pr[sm];
+      pr[sm] = tempProc;
     }
   }
+}
 
-
-
-struct process pr[100];
-//FCFS-------------------------------------------------------------------------------
-
-
-
-  for (int i = 0; i < n; i++){
-    pr[i] = prt[i];
-  }
-  // Calculating Waiting time and Turnaround time of each process
-  twt[0] = 0;
-  tat[0] = pr[0].bt;
-  for (int i = 1; i < n; i++){
-    twt[i] = 0;
-    for (int j = i-1; j >= 0; j--) {
-      twt[i] += pr[j].bt;
+void sortByIndex(int n){
+  int sm;
+  struct process tempProc;
+  for (int i = 0; i < n-1; i++){
+    sm = i;
+    for (int j = i+1; j < n; j++){
+      if (pr[j].index < pr[sm].index) {
+        sm = j;
+      }
     }
-    twt[i] -= pr[i].at;
-    tat[i] = twt[i] + pr[i].bt;
+    if (sm != i) {
+      tempProc = pr[i];
+      pr[i] = pr[sm];
+      pr[sm] = tempProc;
+    }
   }
-  // Calculating Average Waiting time and Average Turnaround time
-  wtTot = 0;
-  taTot = 0;
+}
+
+void printPorcess(int n){
+  sortByIndex(n);
+  printf("\n\nProcess      AT      BT      Priority\n");
+  printf("-------------------------------------");
   for (int i = 0; i < n; i++){
-    wtTot+=twt[i];
-    taTot+=tat[i];
+    printf("\n  %s          %d      %d          %d", pr[i].pid, pr[i].at, pr[i].bt, pr[i].pt);
   }
+}
 
-  awt = (float)wtTot/n;
-  atat = (float)taTot/n;
-  // Displaying Output
-  printf("\n\n\nFCFS\n------");
-  printf("\n\nAverage Waiting Time : %.2f\n", awt);
-  printf("\nAverage Turn Around Time : %.2f\n\n\n", atat);
-
-
-
-//SJF------------------------------------------------------------------------------
-
-
-
+void fcfs(int n) {
+  sortByAt(n);
+  int st = pr[0].at;   //service time (total time processor worked)
   for (int i = 0; i < n; i++){
-    pr[i] = prt[i];
+    pr[i].wt = st - pr[i].at;
+    st += pr[i].bt;
+    pr[i].tt = st - pr[i].at;
   }
-  int st, top=-1, gtop=-1, done[10], tInd;
-  struct process gnattChart[10], tempChart[10];
-  for(int i = 0; i < n; i++){
-    done[i] = 0;
+  sortByIndex(n);
+  printf("\n\n\n\nOutput (FCFS)\n");
+  printf("-------------");
+  printf("\nProcess      TAT      WT\n");
+  for (int i = 0; i < n; i++){
+    printf("\n  %s          %d       %d", pr[i].pid, pr[i].tt, pr[i].wt);
   }
+}
 
-  // Preparing Gnatt Chart on SJF
+void sjf(int n){
+  sortByAt(n);
+  int st, top=-1, sm, tIndex;
+  struct process tempChart[10], tempProc;
+  for (int i = 0; i < n; i++){
+    pr[i].done = 0;
+  }
   st = pr[0].at;
   for (int k = 0; k < n; k++){
+    // finds all the processes which are currently arrived at the processor
     for (int i = 0; i < n; i++){
-      if (pr[i].at <= st && done[i] != 1) {
+      if (pr[i].at <= st && pr[i].done != 1) {
         tempChart[++top] = pr[i];
       }
     }
-
-    for (int i = 0; i < top; i++) {
+    // sort tempChart by BT
+    for (int i = 0; i < top; i++){
       sm = i;
       for (int j = i+1; j <= top; j++){
         if (tempChart[j].bt < tempChart[sm].bt) {
@@ -117,121 +152,85 @@ struct process pr[100];
         tempChart[sm] = tempProc;
       }
     }
-
-    tInd = tempChart[0].pid;
-    for (int m = 0; m < n; m++){
-      if (pr[m].pid == tInd) {
-        tInd = m;
+    // finds WT and TAT of currently arrived smallest process
+    tIndex = tempChart[0].index;
+    for (int i = 0; i < n; i++){
+      if (pr[i].index == tIndex) {
+        pr[i].wt = st - pr[i].at;
+        st += pr[i].bt;
+        pr[i].tt = st - pr[i].at;
+        pr[i].done = 1;
         break;
       }
     }
-
-    done[tInd] = 1;
-    gnattChart[++gtop]=tempChart[0];
-    st += tempChart[0].bt;
-    top=-1;
+    top = -1;
   }
-
-  // Calculating Waiting time and Turnaround time of each process
-  twt[0] = 0;
-  tat[0] = pr[0].bt;
-  for (int i = 1; i < n; i++){
-    twt[i] = 0;
-    for (int j = i-1; j >= 0; j--) {
-      twt[i] += gnattChart[j].bt;
-    }
-    twt[i] -= gnattChart[i].at;
-    tat[i] = twt[i] + gnattChart[i].bt;
-  }
-  // Calculating Average Waiting time and Average Turnaround time
-  wtTot = 0;
-  taTot = 0;
+  sortByIndex(n);
+  printf("\n\n\n\nOutput (SJF)\n");
+  printf("------------");
+  printf("\nProcess      TAT      WT\n");
   for (int i = 0; i < n; i++){
-    wtTot+=twt[i];
-    taTot+=tat[i];
+    printf("\n  %s          %d       %d", pr[i].pid, pr[i].tt, pr[i].wt);
   }
+}
 
-  awt = (float)wtTot/n;
-  atat = (float)taTot/n;
-  // Displaying Output
-  printf("\n\n\nSJF\n------");
-  printf("\n\nAverage Waiting Time : %.2f\n", awt);
-  printf("\nAverage Turn Around Time : %.2f\n\n\n", atat);
-
-
-
-//Round Robin----------------------------------------------------------
-
-
-
-  int qTime, rear = n-1, arBt[10], tempWt, tempTat, ind;
-  printf("\n\nEnter the Time Quantum for Round Robin : ");
-  scanf("%d", &qTime);
+void roundRobin(int n, int qTime){
+  struct process rr[100];
+  int rear = n-1, st, tIndex;
+  sortByAt(n);
   for (int i = 0; i < n; i++){
-    pr[i] = prt[i];
-    arBt[i] = pr[i].bt;
+    rr[i] = pr[i];
   }
-  st = pr[0].at;
-  wtTot = 0;
-  taTot = 0;
+  st = rr[0].at;
   for (int i = 0; i <= rear; i++){
-    if (pr[i].at <= st && pr[i].bt > 0){
-      if (pr[i].bt >= qTime){
-        pr[i].bt -= qTime;
+    if (rr[i].at <= st && rr[i].bt > 0) {
+      if (rr[i].bt >= qTime) {
+        rr[i].bt -= qTime;
         st += qTime;
       }else{
-        st += pr[i].bt;
-        pr[i].bt = 0;
+        st += rr[i].bt;
+        rr[i].bt = 0;
       }
     }
-    if (pr[i].bt > 0){
-      pr[++rear] = pr[i];
-    }else {
-      ind = pr[i].pid;
+    if (rr[i].bt > 0) {
+      rr[++rear] = rr[i];
+    }else{
+      tIndex = rr[i].index;
       for (int j = 0; j < n; j++){
-        if (prt[j].pid == ind) {
-          ind = j;
+        if (pr[j].index == tIndex) {
+          pr[j].wt = st - pr[j].at - pr[j].bt;
+          pr[j].tt = st - pr[j].at;
           break;
         }
       }
-      tempWt = st - pr[ind].at - arBt[ind];
-      wtTot += tempWt;
-      tempTat = st - pr[ind].at;
-      taTot += tempTat;
     }
   }
-  awt = (float)wtTot/n;
-  atat = (float)taTot/n;
-  // Displaying Output
-  printf("\n\n\nRound Robin\n------");
-  printf("\n\nAverage Waiting Time : %.2f\n", awt);
-  printf("\nAverage Turn Around Time : %.2f\n\n\n", atat);
-
-
-
-//Priority-----------------------------------------------------------------------------
-
-
-
+  sortByIndex(n);
+  printf("\n\n\n\nOutput (Round Robin)\n");
+  printf("--------------------");
+  printf("\nProcess      TAT      WT\n");
   for (int i = 0; i < n; i++){
-    pr[i] = prt[i];
+    printf("\n  %s          %d       %d", pr[i].pid, pr[i].tt, pr[i].wt);
   }
-  top=-1;
-  gtop=-1;
-  for(int i = 0; i < n; i++){
-    done[i] = 0;
-  }
+}
 
-  // Preparing Gnatt Chart on Priority
+void priority(int n){
+  sortByAt(n);
+  int st, top=-1, sm, tIndex;
+  struct process tempChart[10], tempProc;
+  for (int i = 0; i < n; i++){
+    pr[i].done = 0;
+  }
   st = pr[0].at;
   for (int k = 0; k < n; k++){
+    // finds all the processes which are currently arrived at the processor
     for (int i = 0; i < n; i++){
-      if (pr[i].at <= st && done[i] != 1) {
+      if (pr[i].at <= st && pr[i].done != 1) {
         tempChart[++top] = pr[i];
-        // done[i] = 1;
       }
     }
-    for (int i = 0; i < top; i++) {
+    // sort tempChart by Priority
+    for (int i = 0; i < top; i++){
       sm = i;
       for (int j = i+1; j <= top; j++){
         if (tempChart[j].pt < tempChart[sm].pt) {
@@ -244,48 +243,24 @@ struct process pr[100];
         tempChart[sm] = tempProc;
       }
     }
-
-    tInd = tempChart[0].pid;
-    for (int m = 0; m < n; m++){
-      if (pr[m].pid == tInd) {
-        tInd = m;
+    // finds WT and TAT of currently arrived highest priority
+    tIndex = tempChart[0].index;
+    for (int i = 0; i < n; i++){
+      if (pr[i].index == tIndex) {
+        pr[i].wt = st - pr[i].at;
+        st += pr[i].bt;
+        pr[i].tt = st - pr[i].at;
+        pr[i].done = 1;
         break;
       }
     }
-
-    done[tInd] = 1;
-    gnattChart[++gtop]=tempChart[0];
-    st += tempChart[0].bt;
-    top=-1;
+    top = -1;
   }
-
-  // Calculating Waiting time and Turnaround time of each process
-  twt[0] = 0;
-  tat[0] = pr[0].bt;
-  for (int i = 1; i < n; i++){
-    twt[i] = 0;
-    for (int j = i-1; j >= 0; j--) {
-      twt[i] += gnattChart[j].bt;
-    }
-    twt[i] -= gnattChart[i].at;
-    tat[i] = twt[i] + gnattChart[i].bt;
-  }
-  // Calculating Average Waiting time and Average Turnaround time
-  wtTot = 0;
-  taTot = 0;
+  sortByIndex(n);
+  printf("\n\n\n\nOutput (Priority)\n");
+  printf("-----------------");
+  printf("\nProcess      TAT      WT\n");
   for (int i = 0; i < n; i++){
-    wtTot+=twt[i];
-    taTot+=tat[i];
+    printf("\n  %s          %d       %d", pr[i].pid, pr[i].tt, pr[i].wt);
   }
-
-  awt = (float)wtTot/n;
-  atat = (float)taTot/n;
-  // Displaying Output
-  printf("\n\n\nPriority\n------");
-  printf("\n\nAverage Waiting Time : %.2f\n", awt);
-  printf("\nAverage Turn Around Time : %.2f\n\n\n", atat);
-
-
-
-  return 0;
 }
