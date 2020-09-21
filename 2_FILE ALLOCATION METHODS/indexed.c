4d63d1cf59f9f00cdb88a05ed4594df6eeb74716
navaneethmanoj <36037989+navaneethmanoj@users.noreply.github.com>
2020-09-21 17:50:14 +0530

    Update indexed.c

--- 2_FILE ALLOCATION METHODS/indexed.c
+++ 2_FILE ALLOCATION METHODS/indexed.c
@@ -1,95 +1,93 @@
-#include<stdio.h>
+#include <stdio.h>
+void main()
+{
 
-struct file {
-  int len;
-  int index;
-  char content[10];
-  int isAlloc;
-  int space[10];
-};
-
-void clearScreen();
-
-int main(){
-  struct file files[10];
-  int nb, nf, status[100], top = 1, k, tTop;
-  char blocks[100];
-
-  printf("Enter Number of blocks : ");
-	scanf("%d", &nb);
-	printf("Enter Number of files : ");
-	scanf("%d", &nf);
-
-  for (int i = 0; i < nb; i++){
-    status[i] = 1;
-    blocks[i] = '0';
-  }
-
-  for (int i = 0; i < nf; i++){
-		printf("\n\nEnter Length of File %d : ", i+1);
-		scanf("%d", &files[i].len);
-    printf("Enter Index Node of File %d : ", i+1);
-    scanf("%d", &files[i].index);
-		printf("Enter Content of File %d : ", i+1);
-		scanf("%s", files[i].content);
-    files[i].isAlloc = 0;
-	}
-
-/*
-
-  for(int i = 0; i < nf; i++){
-    if (status[files[i].index] == 1 && top <= nb) {
-      status[files[i].index] = 0;
-      tTop = top;
-      for (int j = 0; j < files[i].len; j++){
-        k = 0;
-        while (1) {
-          if (top > nb) {
-            break;
-          }
-          if (status[top] == 1) {
-            files[i].space[k++] = top++;
-            break;
-          }else{
-            top++;
-          }
-        }
-      }
-      top = tTop;
-      if (k != files[i].len){
-        status[files[i].index] = 1;
-      }else{
-        for (int j = 0; j < files[i].len; j++){
-          while (1) {
-            if (status[top] == 1) {
-              status[top] = 0;
-              blocks[top++] = files[i].content[j];
-              break;
-            }else{
-              top++;
+    int nb,nf,nd,i,j,ind,k=0,fr=1,index[10][10]={0};
+    char content[10];
+    int len[10],disp[10],p[10],f[10]={0},req[10]={0};
+    char s[10];
+    printf("\nEnter No of blocks:");
+    scanf("%d",&nb);
+    printf("Enter No of files:");
+    scanf("%d",&nf);
+    for(i=0;i<nf;i++)
+    {
+        printf("Enter Length,index node and contents of file :");
+        scanf("%d%d%s",&len[i],&ind,s);
+        req[i]= ind;
+        if(!f[ind])
+        {
+            f[ind]=1;
+            p[i]=0;
+            for(j=0;j<len[i];j++)
+            {
+                if(!f[fr])
+                {
+                index[ind][j] = fr;
+                content[fr]=s[j];
+                f[fr]=1;
+                fr++;
+                }
+                else{
+                    fr++;
+                }
             }
-          }
         }
-        files[i].isAlloc = 1;
-      }
-    }
-  }
+        else{
+            p[i]=1;
 
-  for (int i = 0; i < nf; i++){
-    printf("%d\t\t", files[i].index);
-    if (files[i].isAlloc == 1) {
-      for (int j = 0; j < files[i].len; j++){
-        printf("%d, ", files[i].space[j]);
-      }
-      printf("\t\tAllocated\n");
-    }else{
-      printf("\t\t\t\tNot Allocated\n");
+        }
     }
-  }
-*/
-  return 0;
-}
+    printf("\n\n");
+    printf("Enter No of files to be displayed :");
+    scanf("%d",&nd);
+    printf("Enter index of the files :");
+    for(i=0;i<nd;i++){
+      scanf("%d",&disp[i]);
+    }
+    printf("\n\n");
+    for(i=0;i<nf;i++)
+    {
+        printf("P%d \t",i+1);
+        if(!p[i])
+        {
+                if(req[i])
+                {
+                    printf("%d\t",req[i]);
+                    j=0;
+                    while(index[req[i]][j]!=0)
+                    {
+                        if(j==0)
+                            printf("%d",index[req[i]][j]);
+                        else
+                            printf(",%d",index[req[i]][j]);
+                        j++;
+                    }
 
-void clearScreen(){
-  printf("\e[1;1H\e[2J");
+                }
+            printf("\tAllocated\n");
+        }
+        else
+            printf("%d\t\tNot Allocated\n",req[i]);
+
+    }
+    printf("Index \t Allocated block \t Content \n");
+    for(i=0;i<nd;i++)
+    {
+        for(k=0;k<nf;k++){
+            if(disp[i]==req[k]){
+                j=0;
+                int l=req[k];
+                while(index[l][j]!=0)
+                {
+                    printf("%d \t",l);
+                    printf("\t%d \t ",index[l][j]);
+                    printf("\t%c \n",content[index[l][j]]);
+                    j++;
+                }
+                break;
+            }
+        }
+
+    }
 }
